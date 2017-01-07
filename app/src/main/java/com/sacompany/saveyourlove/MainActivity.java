d9841182260e01a3f915aa67a6626faf3ecb4097
Arif Sami Tekinkaya <arifsami@live.com>
2017-01-08 01:23:53 +0300

    şablon oluştu. içerikler girilecek artık.

--- app/src/main/java/com/sacompany/saveyourlove/MainActivity.java
+++ app/src/main/java/com/sacompany/saveyourlove/MainActivity.java
@@ -1,24 +1,157 @@
 package com.sacompany.saveyourlove;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public ViewPager viewPager;
-    TextView textView;
-    FragmentManager fragmentManager;
+
+    // soruların sırasını tutar   -- Şu an için 2 soru var
+    int[] soruListesi = new int[]{1, 2};
+    // cevapların sırasını tutar
+    int[] cevapListesi = new int[]{1, 2};
+
+
+    public ViewFlipper viewFlipperTest;
+
+
+    // -- Soru 1 için -- //
+    RadioGroup cevaplarSoru1;
+    LinearLayout layoutHata1;
+    Button btnHata1;
+
+    // -- Soru 2 için -- //
+    RadioGroup cevaplarSoru2;
+    LinearLayout layoutHata2;
+    Button btnHata2;
+
+    // eğer bir hata yapılırsa true olarak işaretlenir ve en başa dönmek için kullanılır.
+    boolean yanlisMi = false;
+
+    public void TumElemanlariTanimla() {
+
+        viewFlipperTest = (ViewFlipper) findViewById(R.id.viewFlipperTest);
+
+        // -- Cevaplar -- //
+        cevaplarSoru1 = (RadioGroup) findViewById(R.id.radioGroupCevap1);
+        cevaplarSoru2 = (RadioGroup) findViewById(R.id.radioGroupCevap2);
+
+        // -- Hatalar -- //
+        layoutHata1 = (LinearLayout) findViewById(R.id.hata1);
+        layoutHata2 = (LinearLayout) findViewById(R.id.hata2);
+
+        // hata buton //
+        btnHata1 = (Button) findViewById(R.id.btnHata1);
+        btnHata2 = (Button) findViewById(R.id.btnHata2);
+
+        // -- cevaplar sırasıyla -- // -- seçili olayı için çözüm olmaz ise tek tek kullanılabilir.
+        RadioButton soru1cevap1 = (RadioButton) findViewById(R.id.cevap1soru1);
+        RadioButton soru1cevap2 = (RadioButton) findViewById(R.id.cevap2soru1);
+        RadioButton soru1cevap3 = (RadioButton) findViewById(R.id.cevap3soru1);
+        RadioButton soru1cevap4 = (RadioButton) findViewById(R.id.cevap4soru1);
+        RadioButton soru2cevap1 = (RadioButton) findViewById(R.id.cevap1soru2);
+        RadioButton soru2cevap2 = (RadioButton) findViewById(R.id.cevap2soru2);
+        RadioButton soru2cevap3 = (RadioButton) findViewById(R.id.cevap3soru2);
+        RadioButton soru2cevap4 = (RadioButton) findViewById(R.id.cevap4soru2);
+
+    }
+
+    public void MesajGoster(String mesaj) {
+        Toast.makeText(this, mesaj, Toast.LENGTH_SHORT).show();
+    }
+
+    private void TumCevaplarinKontrolleri() {
+
+        cevaplarSoru1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                Random rand = new Random();
+                int chance = rand.nextInt(2);
+                if (chance == 1) {
+                    HataGoster();
+                } else {
+                    viewFlipperTest.showNext();
+                }
+                final RadioButton r = (RadioButton) radioGroup.findViewById(i);
+                r.setChecked(false);
+            }
+        });
+
+        cevaplarSoru2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                Random rand = new Random();
+                int chance = rand.nextInt(2);
+                if (chance == 1) {
+                    HataGoster();
+                } else {
+                    viewFlipperTest.showNext();
+                }
+                RadioButton r = (RadioButton) radioGroup.findViewById(i);
+                r.setChecked(false);
+            }
+        });
+
+    }
+
+    private void HataGoster() {
+
+        Random rand = new Random();
+        int chance = rand.nextInt(2);
+
+        MesajGoster("rastgele sayı: " + chance);
+        if (chance == 1) {
+            MesajGoster("2. hata ekranı denk geldi");
+            viewFlipperTest.setVisibility(View.GONE);
+            layoutHata1.setVisibility(View.GONE);
+            layoutHata2.setVisibility(View.VISIBLE);
+
+            btnHata2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MesajGoster("2. hata ekranındaki buton a bastın");
+                    layoutHata2.setVisibility(View.GONE);
+                    layoutHata1.setVisibility(View.GONE);
+                    viewFlipperTest.setVisibility(View.VISIBLE);
+                    viewFlipperTest.setDisplayedChild(0);
+
+                }
+            });
+
+        } else {
+            MesajGoster("1. hata ekranı denk geldi");
+            viewFlipperTest.setVisibility(View.GONE);
+            layoutHata2.setVisibility(View.GONE);
+            layoutHata1.setVisibility(View.VISIBLE);
+
+            btnHata1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MesajGoster("1. hata ekranındaki buton a bastın");
+                    layoutHata2.setVisibility(View.GONE);
+                    layoutHata1.setVisibility(View.GONE);
+                    viewFlipperTest.setVisibility(View.VISIBLE);
+                    viewFlipperTest.setDisplayedChild(0);
+
+                }
+            });
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,45 +160,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //ViewPager Tanımlamaları
-        fragmentManager = getSupportFragmentManager();
-        viewPager=(ViewPager)findViewById(R.id.viewpager);
-        viewPager.setAdapter(new SayfaAdapter(fragmentManager));
+        TumElemanlariTanimla();
+
+        TumCevaplarinKontrolleri();
+
     }
 
-    public class SayfaAdapter extends FragmentPagerAdapter {
-
-        public SayfaAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-
-            Fragment fragment = null;
-            //Bu kısımda hangi sayfada nerenin görüntüleneceğini seçiyoruz.
-            //0 birinci sayfa...
-            if (position == 0) {
-                fragment = new Soru1();
-            } else if (position == 1) {
-                fragment = new Soru2();
-            } else if (position == 2) {
-                fragment = new Hata1();
-            } else if (position == 3) {
-                fragment = new Hata2();
-            }
-
-            return fragment;
-        }
-
-        @Override
-        public int getCount() {
-
-            //kaç sayfa varsa onu yazacaksın
-            return 4;
-
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/sacompany/saveyourlove/Soru1.java
+++ app/src/main/java/com/sacompany/saveyourlove/Soru1.java
@@ -6,18 +6,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by arifsami on 02/01/2017.
  */
 public class Soru1 extends Fragment {
+    RadioGroup cevap1;
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.soru1, container, false);
         //ekleyeceğin görsel elamnları bu view ile ekleyeceksin
         //örneğin burda view.findViewbyId dedik bunun gibi
         //TextView txtSoru1 = (TextView) view.findViewById(R.id.txtSoru1);
         //Bu kısımdan sonra textView.setText diyerek işlemlerini yapabilirsin
+
+        cevap1 = (RadioGroup) view.findViewById(R.id.radioGroupCevap1);
+
+        cevap1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+              //  Toast.makeText(getContext(), "" + radioGroup.getCheckedRadioButtonId() + " - \ni = " + i, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
     }
 
